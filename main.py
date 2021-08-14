2c29b5334dca12901d435af8c387282349df0f18
Jonatan <jonarey165@gmail.com>
2021-08-13 19:50:47 -0600

    lectura de archivo xml

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
-def lecturaarchivo():
-    print("Prueba")
+import xml.etree.ElementTree as ET
 
+
+def lecturaarchivo(ruta):
+    mytree = ET.parse(ruta)
+    myroot = mytree.getroot()
+    for x in myroot[0]:
+        print(x.tag,x.attrib)
 
 def menu():
     ans = True
     while ans:
-        print("---------Bienvenidos---------")
-        print(" 1.Cargar Archivo")
-        print(" 2.Procesar Archivo")
-        print(" 3.Escribir Archivo de Salida")
-        print(" 4.Mostrar Datos del Estudiante")
-        print(" 5.Generar Gráfica")
-        print(" 6.Salir")
+        print("----------------Bienvenidos--------------")
+        print("|    1.Cargar Archivo                   |")
+        print("|    2.Procesar Archivo                 | ")
+        print("|    3.Escribir Archivo de Salida       |")
+        print("|    4.Mostrar Datos del Estudiante     |")
+        print("|    5.Generar Gráfica                  |")
+        print("|    6.Salir                            |")
+        print("-----------------------------------------")
         ans = input("Eliga una opción: ")
         if ans == "1":
-            lecturaarchivo()
-            print("Archivo cargado")
+            ruta = input("Ingrese la ruta del archivo: ")
+            lecturaarchivo(ruta)
+            print("Archivo cargado\n")
         elif ans == "2":
             print("Archivo procesado con exito")
             print()
